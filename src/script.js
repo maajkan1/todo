b83045c7a3b0deea72b3c7c61bd81f119d3f16e1
maajkan <maajkan@live.se>
2025-09-17 14:35:18 +0200

    tasks är nu objects med isDone

--- src/script.js
+++ src/script.js
@@ -12,61 +12,66 @@ const taskbutton = document.querySelector("#todo-button");
 const taskDisplay = document.querySelector("#task-display");
 
 // Logik för att lägga till task
-const addTask = e => {
-    e.preventDefault();
+const addTask = (e) => {
+  e.preventDefault();
 
-    const inputValue = taskInput.value.trim();
-    //Är input = inget så avslutas det
-    if (inputValue === "") return;
-    
-    //Om index inte är 0, så läggs det tasken
-    //man tryckt edit på i input-field
-    if (indexForBtn !== null) {
-        tasks[indexForBtn] = inputValue;
-        indexForBtn = null;
-    } else {
-        //annars pushar man in input i arrayen
-        tasks.push(inputValue);
-    }
-    //återställer input-fielden
-    taskInput.value = "";
-    //renderar om task
-    localStorage.setItem("key", JSON.stringify(tasks));
-    renderTask();
+  const inputValue = taskInput.value.trim();
+  //Är input = inget så avslutas det
+  if (inputValue === "") return;
+
+  //Om index inte är 0, så läggs det tasken
+  //man tryckt edit på i input-field
+  if (indexForBtn !== null) {
+    tasks.name[indexForBtn] = inputValue;
+    indexForBtn = null;
+  } else {
+    //annars pushar man in input i arrayen
+    tasks.push({
+      name: inputValue,
+      isDone: false,
+    });
+  }
+  //återställer input-fielden
+  taskInput.value = "";
+  //renderar om task
+  localStorage.setItem("key", JSON.stringify(tasks));
+  renderTask();
 };
 //Lägger till funktionen till knappen "Skicka"
-taskbutton.addEventListener('click', addTask);
+taskbutton.addEventListener("click", addTask);
 
 const renderTask = () => {
-    taskDisplay.innerHTML = "";
-    //Foreach istället för vanlig loop.
-    tasks.forEach((task, i) => { 
-        let liElement = document.createElement("li");
-        let liEdit = document.createElement("button");
-        let liDelete = document.createElement("button");
+  taskDisplay.innerHTML = "";
+  //Foreach istället för vanlig loop.
+  tasks.forEach((task, i) => {
+    let liElement = document.createElement("li");
+    let liEdit = document.createElement("button");
+    let liDelete = document.createElement("button");
 
-        // Use textContent to add text without overwriting the buttons
-        liElement.textContent = task; 
-        
-        liEdit.innerText = "Edit";
-        liDelete.innerText = "Delete";
+    // Use textContent to add text without overwriting the buttons
+    liElement.textContent = task.name;
 
-        liElement.appendChild(liEdit);
-        liElement.appendChild(liDelete);
-        taskDisplay.appendChild(liElement);
+    liEdit.innerText = "Edit";
+    liDelete.innerText = "Delete";
 
-        liEdit.addEventListener('click', () => {
-            taskInput.value = task;
-            indexForBtn = i;
-        });
+    liElement.appendChild(liEdit);
+    liElement.appendChild(liDelete);
+    taskDisplay.appendChild(liElement);
 
-        liDelete.addEventListener('click', () => {
-            tasks.splice(i, 1);
-            //Sparar i localstorage precis när man tagit bort nåt.
-            //Renderar sidan om igen
-            localStorage.setItem("key", JSON.stringify(tasks));
-            renderTask();
-        });
+    liEdit.addEventListener("click", () => {
+      taskInput.value = task.name;
+      indexForBtn = i;
     });
+
+    liDelete.addEventListener("click", () => {
+      tasks.splice(i, 1);
+      //Sparar i localstorage precis när man tagit bort nåt.
+      //Renderar sidan om igen
+      localStorage.setItem("key", JSON.stringify(tasks));
+      renderTask();
+    });
+  });
 };
 renderTask();
+// console.table(tasks)
+// console.log(tasks[0].isDone);
